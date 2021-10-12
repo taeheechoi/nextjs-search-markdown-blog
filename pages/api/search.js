97fd7ef27454c46b1c883e7e8148c012bab37906
Tae Hee Choi <dadac76@hotmail.com>
2021-10-11 17:54:23 -0700

    fuzzy search

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,9 +1,12 @@
 import { getSortedPostsData } from "../../lib/posts"
+import FuzzySearch from "fuzzy-search"
 
 const posts = process.env.NODE_ENV === 'production' ? require("../../cache/data").posts : getSortedPostsData() 
 
 export default (req, res) => {
-    const results = req.query.q ? posts.filter(post => post.title.toLocaleLowerCase().includes(req.query.q)) : []
+    const searcher = new FuzzySearch(posts, ['title'], {caseSensitive: false})
+    const results = req.query.q ? searcher.search(req.query.q) : []
+    // const results = req.query.q ? posts.filter(post => post.title.toLocaleLowerCase().includes(req.query.q)) : []
     res.status = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({results}))
